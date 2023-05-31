a1358a65de73039a2bbc01f9cf7a1f1032ddfb59
Harry Parkes <harrydparkes@proton.me>
2023-06-01 11:09:31 +1200

    Correctly raising argument errors in Conversions

--- Conversions.py
+++ Conversions.py
@@ -3,10 +3,14 @@ def char_to_int(char: str) -> int:
     Converts single character to numerical value
     :param char: string of length 1 containing a letter
     :return: integer representation of `char`
+    :raises TypeError: If `char` is not a single character (string of length 1)
+    :raises ValueError: If `char` is not a lowercase english letter
     """
     number = ord(char.lower()) - ord('a')
+
     if not 0 <= number < 26:
-        raise ValueError("char must be valid english letter")
+        raise ValueError("char must be a lowercase english letter")
+
     return number
 
 
@@ -15,7 +19,9 @@ def int_to_char(number: int) -> str:
     Converts an integer to a character
     :param number: the integer to convert
     :return: string of length 1 containing letter representation of `number`
+    :raises ValueError: If `number` is not within the bounds [0, 26)
     """
+    if not 0 <= number < 26:
+        raise ValueError("number must be in the range [0, 26)")
 
-    assert 0 <= number < 26
     return chr(ord('a') + number)
