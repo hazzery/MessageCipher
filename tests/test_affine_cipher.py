e855ab94adbde54841194ce17da4db7314acfdfa
Harry Parkes <harrydparkes@proton.me>
2023-09-03 15:04:47 +1200

    Updated tests because they were wrong

--- tests/test_affine_cipher.py
+++ tests/test_affine_cipher.py
@@ -11,10 +11,10 @@ class TestAffineCipher(unittest.TestCase):
 
     def test_invalid_a_coefficient(self):
         """degree_one coefficients not invertible modulo 26."""
-        self.assertRaises(ValueError, AffineCipher, 3, 0)
-        self.assertRaises(ValueError, AffineCipher, 4, 0)
-        self.assertRaises(ValueError, AffineCipher, 9, 0)
-        self.assertRaises(ValueError, AffineCipher, 25, 0)
+        self.assertRaises(ValueError, AffineCipher, 2, 0)
+        self.assertRaises(ValueError, AffineCipher, 6, 0)
+        self.assertRaises(ValueError, AffineCipher, 13, 0)
+        self.assertRaises(ValueError, AffineCipher, 26, 0)
 
         # invertible coefficients shouldn't throw error
         AffineCipher(5, 0)
@@ -35,25 +35,25 @@ class TestAffineCipher(unittest.TestCase):
 
     def test_encrypt(self):
         """Tests basic string encryption, checking uppercase, lowercase, and white-space."""
-        cipher = AffineCipher(7, 7)
+        cipher = AffineCipher(7, 11)
         plaintext = "HELLOWORLD"
         plaintext2 = "hello world"
-        expected_ciphertext = "EJGGBFBWGC"
+        expected_ciphertext = "INKKFJFAKG"
         self.assertEqual(expected_ciphertext, cipher.encrypt(plaintext))
         self.assertEqual(expected_ciphertext, cipher.encrypt(plaintext2))
 
     def test_decrypt(self):
         """Tests basic string decryption, checking uppercase, lowercase, and white-space."""
-        cipher = AffineCipher(7, 7)
-        ciphertext = "EJGGBFBWGC"
-        ciphertext2 = "ejggb fbwgc"
+        cipher = AffineCipher(11, 13)
+        ciphertext = "MFEELVLSEU"
+        ciphertext2 = "mfeel vlseu"
         expected_plaintext = "HELLOWORLD"
         self.assertEqual(expected_plaintext, cipher.decrypt(ciphertext))
         self.assertEqual(expected_plaintext, cipher.decrypt(ciphertext2))
 
     def test_encrypt_decrypt(self):
         """Tests that output of encryption decrypts to its input."""
-        cipher = AffineCipher(11, 19)
+        cipher = AffineCipher(25, 17)
         plaintext = "Test input with spaces"
         ciphertext = cipher.encrypt(plaintext)
         decrypted_plaintext = cipher.decrypt(ciphertext)
