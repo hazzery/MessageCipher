96c339c9f77bab5c5d927a932615ac4e91c5161a
Harry Parkes <harrydparkes@proton.me>
2023-09-06 14:13:23 +1200

    improved main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from src.message_cipher.caesar_cipher import CaesarCipher
 from src.message_cipher.rsa_system import RSA
 
 ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+message = ""
 
 
 def test_cipher(cipher: AbstractCipher):
@@ -17,17 +18,17 @@ def test_cipher(cipher: AbstractCipher):
     Runs the given cipher with some example input
     :param cipher: Any of `CaesarCipher`, `AffineCipher`, or `RSA`
     """
-    message = "Super secretive message"
     ciphertext = cipher.encrypt(message)
     plaintext = cipher.decrypt(ciphertext)
     print('"' + message + '"' + " ->", cipher, "->", ciphertext)
-    print('"' + plaintext + '"' + " <-", cipher, "<-", ciphertext)
+    print('"' + str(ciphertext) + '"' + " ->", cipher, "->", plaintext)
     print()
 
 
 if __name__ == '__main__':
-    test_cipher(AffineCipher(5, 5))
-    test_cipher(AffineCipher(7, 7))
+    message = input("Enter a message to encrypt: ")
+    test_cipher(AffineCipher(5, 13))
+    test_cipher(AffineCipher(7, 19))
 
     test_cipher(CaesarCipher(11))
     test_cipher(CaesarCipher(23))
