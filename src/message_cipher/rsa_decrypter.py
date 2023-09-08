08bf115cdaa53513d3fb38d076793a1ecb80dc0e
Harry Parkes <harrydparkes@proton.me>
2023-09-08 20:58:16 +1200

    new RSA implementations of Encoder and Decoder

---
+++ src/message_cipher/rsa_decrypter.py
@@ -0,0 +1,43 @@
+"""
+The RSA decryption module defines the RsaDecrypter class,
+a concrete implementation of the Decrypter class.
+Uses the RSA decryption algorithm to decrypt messages encrypted with the RSA encryption algorithm.
+"""
+
+from .conversions import int_to_char
+from .decrypter import Decrypter
+
+
+# pylint: disable=locally-disabled, too-few-public-methods
+class RsaDecrypter(Decrypter):
+    """RsaDecrypter performs calculations to decrypt arrays of integers into string messages"""
+
+    def __init__(self, product: int, private_key: int):
+        """
+        Initializes a new RSA decryption system with values `product` and `private_key`.
+        :param product: The product of two large prime numbers.
+        :param private_key: The private key used for decryption.
+        """
+        self.product = product
+        self.__private_key = private_key
+
+    def _decrypt_num(self, num: int) -> str:
+        """
+        Decrypts a single character using the RSA system.
+        :param num: An integer to be decrypted.
+        :return: A string of length 1 containing the decrypted letter.
+        """
+        number = pow(num, self.__private_key, self.product)
+        return int_to_char(number).upper()
+
+    def decrypt(self, ciphertext: list[int]) -> str:  # type: ignore[override]
+        """
+        Decrypts the encrypted message using the RSA system.
+        :param ciphertext: An encrypted message to decrypt.
+            The message should be a list of integers, as per output of `RsaEncrypter.encrypt`.
+        :return: The decrypted message as a string.
+        """
+        plaintext = ""
+        for num in ciphertext:
+            plaintext += self._decrypt_num(num)
+        return plaintext
---
+++ src/message_cipher/rsa_encrypter.py
@@ -0,0 +1,44 @@
+"""
+The RSA encryption module defines the RsaEncrypter class,
+a concrete implementation of the Encrypter class.
+Uses the RSA encryption algorithm to encrypt string messages.
+"""
+
+from .conversions import char_to_int
+from .encrypter import Encrypter
+
+
+# pylint: disable=locally-disabled, too-few-public-methods
+class RsaEncrypter(Encrypter):
+    """RsaEncrypter performs calculations to encrypt string messages"""
+
+    def __init__(self, product: int, exponent: int):
+        """
+        Initializes a new RSA decrypter with specified `exponent` and `product`.
+        :param product: The product of two large prime numbers.
+        :param exponent: The exponent used for encryption.
+        """
+        self.exponent = exponent
+        self.product = product
+
+    def _encrypt_char(self, char: str) -> int:
+        """
+        Encrypts a single character using the RSA system.
+        :param char: A string of length 1 containing the letter to be encrypted.
+        :return: A string of length 1 containing the encrypted letter.
+        """
+        number = pow(char_to_int(char), self.exponent, self.product)
+        return number
+
+    def encrypt(self, plaintext: str) -> list:
+        """
+        Encrypts a message using the RSA system.
+        :param plaintext: A message to be encrypted.
+        :return: The encrypted message as an array of integers.
+        """
+
+        cipher_array = []
+        for char in plaintext:
+            if char.isalpha():
+                cipher_array.append(self._encrypt_char(char))
+        return cipher_array
