90cb75d86bbed21baa5822f027ed0688712f61f4
Harry Parkes <harrydparkes@proton.me>
2023-09-08 21:34:05 +1200

    made cipher.py pylint compliant

--- src/message_cipher/cipher.py
+++ src/message_cipher/cipher.py
@@ -1,3 +1,6 @@
+"""
+The cipher module defines the Cipher abstract base class.
+"""
 from abc import ABCMeta
 
 from .encrypter import Encrypter
@@ -5,4 +8,7 @@ from .decrypter import Decrypter
 
 
 class Cipher(Encrypter, Decrypter, metaclass=ABCMeta):
-    pass
+    """
+    The Cipher class acts as an interface for objects that can both encrypt and decrypt messages.
+    It allows for polymorphism between the different types of ciphers.
+    """
